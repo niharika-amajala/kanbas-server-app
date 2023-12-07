e4562d1f174e60d0b9d4bbfc28eb403cba936983
niharika-amajala <niharika.amajala@gmail.com>
2023-12-07 15:10:08 -0500

    MongoDb  connection

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
-import "dotenv/config";
-import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./Lab5.js";
-import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./modules/routes.js";
-import AssignmentRoutes from "./assignments/routes.js";
-import cors from "cors";
-
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import Hello from './hello.js';
+import Lab5 from './Lab5.js';
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
+import UserRoutes from "./users/routes.js";
 const app = express();
-app.use(
-    cors()
-);
+const port = process.env.PORT || 4000;
+
+app.use(cors());
 app.use(express.json());
+
+
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000);
 
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
+
+
+process.on('SIGINT', () => {
+  db.close(() => {
+    console.log('Disconnected from MongoDB');
+    process.exit();
+  });
+});
